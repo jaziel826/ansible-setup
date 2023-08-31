55d970477760f73158e6b321c150c9e9026d40f2
Jaziel Amadiz <jaziel.amadiz@pm.me>
2023-08-30 21:45:24 -0400

    added alert python script

---
+++ files/alert.py
@@ -0,0 +1,31 @@
+import sys
+import requests
+
+def send_alert(data, title, priority, tags):
+    url = "https://ntfy.sh/Ansible"
+    headers = {
+        "Title": title,
+        "Priority": priority,
+        "Tags": tags
+    }
+    response = requests.post(url, data=data, headers=headers)
+    
+    if response.status_code == 200:
+        print("Alert sent successfully!")
+    else:
+        print("Failed to send alert.")
+
+def main():
+    if len(sys.argv) != 5:
+        print("Usage: python alert_script.py <data> <title> <priority> <tags>")
+        return
+    
+    data = sys.argv[1]
+    title = sys.argv[2]
+    priority = sys.argv[3]
+    tags = sys.argv[4]
+    
+    send_alert(data, title, priority, tags)
+
+if __name__ == "__main__":
+    main()
